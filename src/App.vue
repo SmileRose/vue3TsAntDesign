f9398a72d446b5105815e0906537ee21aae1836a
Rose <hey_cool@163.com>
2021-02-09 14:56:21 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>  
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
